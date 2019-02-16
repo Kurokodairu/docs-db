90ca6c5a1368c436a3e836ab32de99cfdd233c27
Kurokodairu <jsvendsli@gmail.com>
2019-02-16 20:00:05 +0100

    + Everything

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const server = require('server');
-const { render } = server.reply;
+var fs = require('fs');
+const { get, post } = server.router;
+const { render, json } = server.reply;
 
-const id = '1mUGtudrnhMV7JbzHFuWPPbHOkKYk6KLQaNPWrlyejSg';
+const id = "1mUGtudrnhMV7JbzHFuWPPbHOkKYk6KLQaNPWrlyejSg"
 const drive = require('drive-db')(id);
 
-server(async () => {
+const homeRoute = get('/', async () => {
   const db = await drive.load();
-
+  console.log(db.find());
   return render('index.hbs', { users: db.find() });
 });
+
+server(25566, homeRoute);
+
+function deleteCache() {
+  fs.unlink("db.json", function (err) {
+    if (err) throw err;
+    console.log("DB Cache Deleted!");
+  });
+}
+
+setInterval(function(){
+  deleteCache();
+}, 5000);
